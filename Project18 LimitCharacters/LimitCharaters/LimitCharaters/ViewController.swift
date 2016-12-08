2aa4782b0bb9ba6be430050b6b207306fda01940
陈佐 <chenzuo0730@outlook.com>
2016-12-08 15:43:00 +0800

    Project18

---
+++ Project18 LimitCharacters/LimitCharaters/LimitCharaters/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  LimitCharaters
+//
+//  Created by ChenZuo on 2016/12/8.
+//  Copyright © 2016年 ChenZuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextViewDelegate{
+
+    @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.delegate = self
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.width/2
+        tweetTextView.backgroundColor = UIColor.clear
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyBoardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyBoardWillHidden(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let myTextViewString = tweetTextView.text
+        characterCountLabel.text = "\(140 - (myTextViewString?.characters.count)!)"
+        if range.length > 140 {
+            return false
+        }
+        let newLength = (myTextViewString?.characters.count)! + range.length
+        return newLength < 140
+    }
+    func keyBoardWillShow(_ note:Notification) {
+        let userInfo = note.userInfo
+        let keyBoardBounds = (userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let duration = (userInfo![UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        let deltaY = keyBoardBounds.size.height
+        let animations:(() -> Void) = {
+            self.bottomView.transform = CGAffineTransform(translationX: 0, y: -deltaY)
+        }
+        if duration > 0 {
+            let options = UIViewAnimationOptions(rawValue: UInt((userInfo![UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).intValue << 16))
+            
+            UIView.animate(withDuration: duration, delay: 0, options:options, animations: animations, completion: nil)
+        }else {
+            animations()
+        }
+    }
+    func keyBoardWillHidden(_ note:Notification) {
+        
+        let userInfo  = note.userInfo
+        let duration = (userInfo![UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        let animations:(() -> Void) = {
+            self.bottomView.transform = CGAffineTransform.identity
+        }
+        if duration > 0 {
+            let options = UIViewAnimationOptions(rawValue: UInt((userInfo![UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).intValue << 16))
+            UIView.animate(withDuration: duration, delay: 0, options:options, animations: animations, completion: nil)
+        }else{
+            animations()
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
